2acd64c3a0a64e3702979114468900d63d7e9495
IvoTsochev <ivo.tsochev@live.com>
2020-07-16 23:51:32 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,86 +1,65 @@
-document.addEventListener("DOMContentLoaded", function(){
 
-// MY WORK PAGE
-const dennyConstruction = document.querySelector('#dennyconstruction');
-const hikov = document.querySelector('#hikov');
-const vasilMatin = document.querySelector('#vasilmatin');
-const motoGp = document.querySelector('#motogp');
-const v2 = document.querySelector('#v2');
-const v1 = document.querySelector('#v1');
 
-const rightWork = document.querySelector('.right-work');
+document.addEventListener("DOMContentLoaded", function () {
 
-// Denny Listener
-dennyConstruction.addEventListener('mouseover', dennyIn);
-// dennyConstruction.addEventListener('mouseout', dennyOut);
+	const swup = new Swup();
+	init();
+	swup.on('contentReplaced', init);
 
-// Hikov Listener
-hikov.addEventListener('mouseover', hikovIn);
-// hikov.addEventListener('mouseout', hikovOut);
 
-// Vasil Matin Listener
-vasilMatin.addEventListener('mouseover', vasilMatinIn);
-// vasilMatin.addEventListener('mouseout', vasilMatinOut);
+	function init() {
+		if (document.querySelector('.my-work')) {
+			// MY WORK PAGE
+			const dennyConstruction = document.querySelector('#dennyconstruction');
+			const hikov = document.querySelector('#hikov');
+			const vasilMatin = document.querySelector('#vasilmatin');
+			const motoGp = document.querySelector('#motogp');
+			const v2 = document.querySelector('#v2');
+			const v1 = document.querySelector('#v1');
 
-// MotoGP Fan Shop Listener
-motoGp.addEventListener('mouseover', motoGpIn);
-// motoGp.addEventListener('mouseout', motoGpOut);
+			const rightWork = document.querySelector('.right-work');
 
-// V2 Listener
-v2.addEventListener('mouseover', v2In);
-// v2.addEventListener('mouseout', v1Out);
+			// Denny Listener
+			dennyConstruction.addEventListener('mouseover', dennyIn);
+			// Hikov Listener
+			hikov.addEventListener('mouseover', hikovIn);
+			// Vasil Matin Listener
+			vasilMatin.addEventListener('mouseover', vasilMatinIn);
+			// MotoGP Fan Shop Listener
+			motoGp.addEventListener('mouseover', motoGpIn);
+			// V2 Listener
+			v2.addEventListener('mouseover', v2In);
+			// V1 Listener
+			v1.addEventListener('mouseover', v1In);
 
-// V1 Listener
-v1.addEventListener('mouseover', v1In);
-// v1.addEventListener('mouseout', v1Out);
-
-// Denny function
-function dennyIn() {
-	rightWork.style.backgroundImage = "url('img/dennyconstruction.png')";
-}
-// function dennyOut() {
-// 	rightWork.style.backgroundImage = 'none';
-// }
-
-// Hikov function
-function hikovIn() {
-	rightWork.style.backgroundImage = "url('img/hikov.png')";
-}
-// function hikovOut() {
-// 	rightWork.style.backgroundImage = 'none';
-// }
-
-// Vasil Matin function
-function vasilMatinIn() {
-	rightWork.style.backgroundImage = "url('img/vasilmatin.png')";
-}
-// function vasilMatinOut() {
-// 	rightWork.style.backgroundImage = 'none';
-// }
-
-// Moto Gp function
-function motoGpIn() {
-	rightWork.style.backgroundImage = "url('img/motogp-fan-shop.png')";
-}
-// function motoGpOut() {
-// 	rightWork.style.backgroundImage = 'none';
-// }
-
-// V2 function
-function v2In() {
-	rightWork.style.backgroundImage = "url('img/v2.png')";
-}
-// function v2Out() {
-// 	rightWork.style.backgroundImage = 'none';
-// }
-
-// V1 function
-function v1In() {
-	rightWork.style.backgroundImage = "url('img/v1.png')";
-}
-// function v1Out() {
-// 	rightWork.style.backgroundImage = 'none';
-// }
+			// Denny function
+			function dennyIn() {
+				rightWork.style.backgroundImage = "url('img/dennyconstruction.png')";
+				console.log('denny')
+			}
+			// Hikov function
+			function hikovIn() {
+				rightWork.style.backgroundImage = "url('img/hikov.png')";
+			}
+			// Vasil Matin function
+			function vasilMatinIn() {
+				rightWork.style.backgroundImage = "url('img/vasilmatin.png')";
+			}
+			// Moto Gp function
+			function motoGpIn() {
+				rightWork.style.backgroundImage = "url('img/motogp-fan-shop.png')";
+			}
+			// V2 function
+			function v2In() {
+				rightWork.style.backgroundImage = "url('img/v2.png')";
+			}
+			// V1 function
+			function v1In() {
+				rightWork.style.backgroundImage = "url('img/v1.png')";
+			}
+		}
+		
+	}
 
 
 
